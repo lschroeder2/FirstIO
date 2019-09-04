0212435f0be97f3217f41baa75cbe5a7e2d21802
lschroeder2 <35712473+lschroeder2@users.noreply.github.com>
2019-09-04 16:30:17 -0700

    Add files via upload
    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
+//Author:Lyell Schroeder
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Lyell";
+    std::cout << "What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
